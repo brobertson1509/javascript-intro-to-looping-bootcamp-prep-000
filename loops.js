e61fb4401cdbd182777b22ece22c6936acf29a35
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 18:53:35 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -8,12 +8,12 @@ if(i=1) {console.log("I am 1 strange loop.")
 }
 
 let i = 0;
- 
+
 function incrementVariable() {
   i = i + 1;
   return i;
 }
- 
+
 do {
   console.log("I run once regardless.");
 } while (incrementVariable() < 5);
