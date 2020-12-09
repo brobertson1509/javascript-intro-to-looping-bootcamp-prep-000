76109bd0e8c0e7e76f3d6417fc0ec19b35d3b5e1
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 19:00:57 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -12,8 +12,10 @@ let i = 0;
 function incrementVariable() {
   i = i + 1;
   return i;
-}
+
+function doWhileLoop(n) {
 
 do {
   console.log("I run once regardless.");
-} while (incrementVariable() < 0);
+} while (incrementVariable() < ${n});
+}
