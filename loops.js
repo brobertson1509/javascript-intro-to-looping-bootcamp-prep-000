209965f904583ed54706a0c01c587bef40bf46a4
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 19:53:26 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -16,3 +16,14 @@ while (n>0){
 }
 return "done";
 }
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5
+  }
+
+  do {
+    array.pop();
+  } while (array.length > -1 && maybeTrue());
+    return array;
+}
