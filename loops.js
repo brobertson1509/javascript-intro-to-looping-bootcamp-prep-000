6336f6d529180b7afd4b87620694db234cf4b97b
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 18:50:49 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,7 +1,7 @@
 let array = [1,2,3,4,5,6,7,8,9,10];
 
 function forLoop(array){
-  for (let i=0; i<number.length, i++) {
+  for (let i=0; i<array.length, i++) {
 if(i=1) {console.log("I am 1 strange loop.")
   }
   else {console.log("I am ${i} strange loops.")}
