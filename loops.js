bfc40d75c5eac37b32ead5f392ccd7bf6c2be3bb
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 19:18:36 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,9 +2,10 @@ let array = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25];
 
 function forLoop(array){
   for (let i=0; i<array.length; i++) {
-if(i=1) {console.log("I am 1 strange loop.")
-  }
-  else {console.log("I am ${i} strange loops.")}
+    if(i=1) {console.log("I am 1 strange loop.")
+      }
+      else {console.log("I am ${i} strange loops.")}
+      return forLoop(array);
 }
 
 function whileLoop(n){
