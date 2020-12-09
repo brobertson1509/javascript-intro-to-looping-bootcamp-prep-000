6c281ba6bcf411857f36d773a46e3bfac7b48a18
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 19:36:18 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,34 +1,11 @@
-function forLoop(array){
-  for (let i=0; i<25; i++) {
-    if(i===1) {
-      array.push(`I am 1 strange loop.`)
-      } else {
-        array.push(`I am ${i} strange loops.`)
-      }
-}
-      return array;
-}
-
-function whileLoop(n){
-
-while (n>0){
-  console.log(--n);
-}
-return "done";
+function forLoop(array) {
+   for ( let i = 0; i < 25; i++) {
+     if (i === 1) {
+       array.push(`I am 1 strange loop.`)
+     } else {
+       array.push(`I am ${i} strange loops.`)
+     }
+  }
+  return array;
 }
 
-let i = 0;
-
-function incrementVariable() {
-  i = i + 1;
-  return i;
-
-function doWhileLoop(num) {
-if(num=0) {
-do {
-  console.log("I run once regardless.");
-} while (incrementVariable() < num);
-else if (num=10){
-  console.log("I run once regardless." )}
-while (incrementVariable() < num);
-}
