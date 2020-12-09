395f67a5d17430e919c23ca91f503f2e1ab81ff6
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 18:49:08 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,11 +2,10 @@ let array = [1,2,3,4,5,6,7,8,9,10];
 
 function makeArray(number){
   for (let i=0; i<number.length, i++) {
-
+if(i=1) {console.log("I am 1 strange loop.")
   }
+  else {console.log("I am ${i} strange loops.")}
 }
-
-let i = 0;
 
 function incrementVariable(){
   i = i +1;
