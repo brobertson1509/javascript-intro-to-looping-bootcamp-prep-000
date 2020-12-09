0dcab4753940c949cd77978286af5b4458766f91
dandy-collaborator-4866 <brobertson@utexas.edu>
2020-12-09 19:28:46 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -5,7 +5,7 @@ function forLoop(array){
     if(i===1) {push.array("I am 1 strange loop.")
       }
       else {array.push("I am ${i} strange loops.")}
-      return forLoop(array);
+      return array;
 }
 
 function whileLoop(n){
